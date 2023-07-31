c9b0b8a537279a8ed78b182759d6148f2d5b96f3
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2023-07-31 09:02:27 +0000

    update PINN_Model class

--- pinn_num.py
+++ pinn_num.py
@@ -2,12 +2,22 @@ import tensorflow as tf
 
 class PINN_Model(tf.keras.Model): 
 
-    def __init__(self,input_dim,output_dim,layers = [20, 20, 20]):
+    def __init__(self,input_dim,output_dim,layers = [20, 20, 20],**kwargs):
         super().__init__(**kwargs)
         self.input_layer = tf.keras.layers.InputLayer(input_shape = (input_dim,1))
         self.output_layer = tf.keras.layers.Dense(output_dim)
         self.hidden_layers = [tf.keras.layers.Dense(n_units, activation = 'tanh') for n_units in layers]
+    
+    def call(self,input):
+        x = np.input_layer(x)
+        for layer in self.hidden_layers : 
+            x = layer(x)
+        output = self.output_layer(x)
+        return output
 
 if __name__ == '__main__':
     print('hello')
+    model = PINN_Model(2,3)
+    model.build()
+    model.summary()
     
