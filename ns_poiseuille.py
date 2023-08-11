1452749030915ce28f6aa9646b8ab4cf88cd1c06
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2023-08-11 13:45:38 +0000

    update ns_poiseuille

--- ns_poiseuille.py
+++ ns_poiseuille.py
@@ -1,4 +1,13 @@
-from pinn_num import * 
+#from pinn_num import * 
+import tf_silent
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.gridspec import GridSpec
+from pinn import PINN
+from network import Network
+from optimizer import L_BFGS_B
 
 def get_data(L, h, u_in):
     # Generate boudary data (X_data, Y_data)
@@ -56,69 +65,126 @@ def get_data(L, h, u_in):
 
     return X_r, X_data, Y_data
 
+def uv(network, xy):
+    """
+    Compute flow velocities (u, v) for the network with output (psi, p).
 
-def main():
-    u_in = 0.034
+    Args:
+        xy: network input variables as ndarray.
+
+    Returns:
+        (u, v) as ndarray.
+    """
+
+    xy = tf.constant(xy)
+    with tf.GradientTape() as g:
+        g.watch(xy)
+        psi_p = network(xy)
+    psi_p_j = g.batch_jacobian(psi_p, xy)
+    u =  psi_p_j[..., 0, 1]
+    v = -psi_p_j[..., 0, 0]
+    return u.numpy(), v.numpy()
+
+def contour(grid, x, y, z, title, levels=50):
+    """
+    Contour plot.
+
+    Args:
+        grid: plot position.
+        x: x-array.
+        y: y-array.
+        z: z-array.
+        title: title string.
+        levels: number of contour lines.
+    """
+
+    # get the value range
+    vmin = np.min(z)
+    vmax = np.max(z)
+    # plot a contour
+    plt.subplot(grid)
+    plt.contour(x, y, z, colors='k', linewidths=0.2, levels=levels)
+    plt.contourf(x, y, z, cmap='rainbow', levels=levels, norm=Normalize(vmin=vmin, vmax=vmax))
+    plt.title(title)
+    cbar = plt.colorbar(pad=0.03, aspect=25, format='%.0e')
+    cbar.mappable.set_clim(vmin, vmax)
+
+def main(maxiter = 2000, num_train_samples = 10000, num_test_samples = 100): 
+    """
+    Test the physics informed neural network (PINN) model
+    for the cavity flow governed by the steady Navier-Stokes equation.
+    """
+
+    u0 = 0.034
     h = 0.05
     L = 20*h
     #Init data
     rho = 910
     mu = 0.3094
     nu = mu/rho
-    print('hello')
-    # Set Model 
-    #model = NN_Model(3, layers= [50, 50, 50, 50, 50])
-    model = get_model(2,3,layers = [30,30,30])
-    # Get domain data and collocation points 
-    colloc_point, X, Y = get_data(L, h, u_in)
-    print(np.shape(colloc_point))
-    print(np.shape(X))
-    # Set PDE parameters 
-    # Set solver 
-    solver = StatNS_PINN(model,colloc_point, nu,pde_residual_scaling = 1.)
-    solver.model.summary()
-    # Set data points 
-    print(solver.input_dim)
-    print(np.shape(Y))
-    print('hello')
-    # Set optimizer 
-    optim = tf.keras.optimizers.Adam(learning_rate = 1e-3)
-    # Solve 
-    solver.solve_with_TFoptimizer(optim, X, Y, N=10)
-    print('run solver model')
-    solver.solve_with_ScipyOptimizer(X, Y,
-                                     method='L-BFGS-B',
-                                     options={'maxiter': 25000,
-                                              'maxfun': 50000,
-                                              'maxcor': 50,
-                                              'maxls': 50,
-                                              'ftol': 1.0*np.finfo(float).eps,
-                                              'pgtol' : 1.e-10})
-    # Plot resulting fields 
-    m_tmp1 = 100
-    m_tmp2 = 30
-    x_tmp = np.linspace(0,L,m_tmp1+2)
-    y_tmp = np.linspace(0,h,m_tmp2+2)
-    x_tmp, y_tmp = np.meshgrid(x_tmp,y_tmp)
-    x_tmp = x_tmp.flatten()
-    y_tmp = y_tmp.flatten()
-    X_disp = np.concatenate((np.expand_dims(x_tmp,1),np.expand_dims(y_tmp,1)),axis = 1)
 
-    pred = solver.model(X_disp)
-    u, v, p = pred[:,0], pred[:,1], pred[:,2]
+    # build a core network model
+    network = Network().build()
+    network.summary()
+    # build a PINN model
+    pinn = PINN(network, rho=rho, nu=nu).build()
 
+    # create training input
+    xy_eqn = np.random.rand(num_train_samples, 2)
+    xy_eqn[:,0] = xy_eqn[:,0] * L
+    xy_eqn[:,1] = xy_eqn[:,1] * h
+    xy_ub = np.random.rand(num_train_samples//2, 2)  # top-bottom boundaries
+    xy_ub[..., 1] = np.round(xy_ub[..., 1])          # y-position is 0 or 1 
+    xy_ub[:,0] = xy_ub[:,0] * L 
+    xy_ub[:,1] = xy_ub[:,1] * h
+    xy_lr = np.random.rand(num_train_samples//2, 2)  # left-right boundaries
+    xy_lr[..., 0] = 0. #np.round(xy_lr[..., 0])          # x-position is 0 
+    xy_lr[:,0] = xy_lr[:,0] * L
+    xy_lr[:,1] = xy_lr[:,1] * h
+    xy_bnd = np.random.permutation(np.concatenate([xy_ub, xy_lr]))
+    x_train = [xy_eqn, xy_bnd] 
 
-    plt.scatter(x_tmp,y_tmp,c = p)
-    plt.colorbar()
+    # create training output
+    zeros = np.zeros((num_train_samples, 2))
+    uv_bnd = np.zeros((num_train_samples, 2))
+    uv_bnd[..., 0] = u0 * np.floor(xy_bnd[..., 1])
+    y_train = [zeros, zeros, uv_bnd]
+
+    plt.scatter(xy_eqn[:,0],xy_eqn[:,1],c = 'black')
+    plt.scatter(xy_bnd[:,0],xy_bnd[:,1],c = uv_bnd[:,0])
     plt.show()
 
-    plt.scatter(x_tmp,y_tmp,c = u)
-    plt.colorbar()
+    plt.scatter(xy_eqn[:,0],xy_eqn[:,1],c = 'black')
+    plt.scatter(xy_bnd[:,0],xy_bnd[:,1],c = uv_bnd[:,1])
     plt.show()
 
-    plt.scatter(x_tmp,y_tmp,c = v)
-    plt.colorbar()
+    # train the model using L-BFGS-B algorithm
+    lbfgs = L_BFGS_B(model=pinn, x_train=x_train, y_train=y_train, maxiter=maxiter)
+    #lbfgs.fit()
+
+    # create meshgrid coordinates (x, y) for test plots
+    x = np.linspace(0, 1, num_test_samples)
+    y = np.linspace(0, 1, num_test_samples)
+    x, y = np.meshgrid(x, y)
+    xy = np.stack([x.flatten(), y.flatten()], axis=-1)
+    # predict (psi, p)
+    psi_p = network.predict(xy, batch_size=len(xy))
+    psi, p = [ psi_p[..., i].reshape(x.shape) for i in range(psi_p.shape[-1]) ]
+    # compute (u, v)
+    u, v = uv(network, xy)
+    u = u.reshape(x.shape)
+    v = v.reshape(x.shape)
+
+    # plot test results
+    fig = plt.figure(figsize=(6, 5))
+    gs = GridSpec(2, 2)
+    contour(gs[0, 0], x, y, psi, 'psi')
+    contour(gs[0, 1], x, y, p, 'p')
+    contour(gs[1, 0], x, y, u, 'u')
+    contour(gs[1, 1], x, y, v, 'v')
+    #plt.tight_layout()
     plt.show()
 
-if __name__ == '__main__' : 
-    main()
+
+if __name__ == '__main__':
+    main(maxiter = 2000, num_train_samples = 10000, num_test_samples = 100)
